5c1cc0eb34ca79051e9b12b391a886a7929ac08f
hikmathuseynov <hikmat.huseynov1212@gmail.com>
2024-06-21 17:04:14 +0400

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Page() {
           >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              Next.js Learn Course sa
             </a>
             , brought to you by Vercel.
           </p>
